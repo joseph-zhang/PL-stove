5210f5301b6897d8b44bf3619fc2888dc3e9ff99
metaphia <zyt5002@gmail.com>
2017-10-16 12:53:47 +0800

    Add files via upload

---
+++ Lipovaca/ch6/Geometry/Sphere.hs
@@ -0,0 +1,11 @@
+-- This file is the subModule of Geometry Module
+module Geometry.Sphere 
+( volume
+, area
+) where
+
+volume :: Float -> Float
+volume radius = (4.0 / 3.0) * pi * (radius ^ 3)
+
+area :: Float -> Float
+area radius = 4 * pi * (radius ^ 2)
