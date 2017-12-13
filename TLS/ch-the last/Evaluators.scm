4cad1436e9224e53bad1909a0fceb74906d34315
metaphia <zyt5002@gmail.com>
2017-12-13 15:51:18 +0800

    Add files via upload

---
+++ TLS/ch-the last/Evaluators.scm
@@ -0,0 +1,3 @@
+; this chapter gives an example of Evaluators,
+; it is interesting, but I'm not going to show it here,
+; some more systematic and deeper books like EoPL will help us learn this part better.
