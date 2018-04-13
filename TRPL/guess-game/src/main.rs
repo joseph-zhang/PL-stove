843fce0ace62e8186bef51dff7a03accb87ad5f9
Metaphia <zyt5002@gmail.com>
2018-04-13 00:36:54 -0500

    add trpl folder

---
+++ TRPL/guess-game/src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+	loop{
+    	println!("Guess the number!");
+		
+		let secret_number = rand::thread_rng().gen_range(1, 101);	
+	
+   	 	println!("Please input your guess.");
+
+    	let mut guess = String::new();
+
+    	io::stdin().read_line(&mut guess)
+        	.expect("Failed to read line");
+
+		let guess: u32 = match guess.trim().parse(){
+			Ok(num) => num,
+			Err(_)  => continue,
+		};
+
+    	println!("You guessed: {}", guess);
+
+		match guess.cmp(&secret_number){
+			Ordering::Less    => println!("samller than {}", secret_number),
+			Ordering::Greater => println!("bigger than {}", secret_number),
+			Ordering::Equal   => {
+				println!("You win!!");
+				break;
+			}
+		}
+	}
+}
