904032c5d6e60e2fac9a840068a077c1f17c581d
Metaphia <zyt5002@gmail.com>
2018-04-14 20:02:28 +0800

    Update main.rs

--- TRPL/guess-game/src/main.rs
+++ TRPL/guess-game/src/main.rs
@@ -4,33 +4,29 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn main() {
-	loop{
-    	println!("Guess the number!");
-		
-		let secret_number = rand::thread_rng().gen_range(1, 101);	
-	 	
-		println!("Please input your guess.");
+fn main(){
+    loop{
+        println!("Guess the number!");
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+        println!("Please input your guesss.");
+        let mut guess = String::new();
 
-    	let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    	io::stdin().read_line(&mut guess)
-        	.expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
 
-		let guess: u32 = match guess.trim().parse(){
-			Ok(num) => num,
-			Err(_)  => continue,
-		};
-
-    	println!("You guessed: {}", guess);
-
-		match guess.cmp(&secret_number){
-			Ordering::Less    => println!("samller than {}", secret_number),
-			Ordering::Greater => println!("bigger than {}", secret_number),
-			Ordering::Equal   => {	
-				println!("You win!!");
-				break;
-			}	
-		}
-	}
+        println!("you guessed: {}", guess);
+        match guess.cmp(&secret_number){
+            Ordering::Less     => println!("smaller than {}", secret_number),
+            Ordering::Greater => println!("bigger than {}", secret_number),
+            Ordering::Equal   => {
+                println!("You win!!");
+                break;
+            }
+        }
+    }
 }
