623b67a52e2a402e5cf98ed48c1c3b4c74ee97cb
joseph-zhang <zyt5002@126.com>
2018-06-24 15:25:12 +0800

    add new instance 'Num'

---
+++ ALJAFP/ch1/Num.java
@@ -0,0 +1,25 @@
+// define a new datatype Num
+abstract class NumD {}
+
+// a value of NumD
+class Zero extends NumD {}
+
+class OneMoreThan extends NumD{
+    NumD predecessor;
+    OneMoreThan(NumD _p){
+	predecessor = _p;
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+	new Zero();
+	
+	new OneMoreThan(
+	 new Zero());
+
+	new OneMoreThan(
+	 new OneMoreThan(
+	  new Zero()));
+    }
+}
