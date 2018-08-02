85c4b324b4383c5c267315cdf020c5f930ececd2
joseph-zhang <zyt5002@126.com>
2018-08-02 17:05:52 +0800

    add 3.50 to ch3

---
+++ SICP/ch3/exm/3.50.scm
@@ -0,0 +1,10 @@
+; 3.50 shows an extension of stream-map
+; it can deal with multiple stream
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
